4c7791949daa2be2c91378c01225e88925b2dad9
Denis <0den4ikys0@gmail.com>
2026-09-08 18:08:31 +0300

    Added patch and delete for tasks and categories CRUD

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
@@ -18,10 +18,14 @@ app.add_middleware(
 class Task(BaseModel):
     id: str
     title: str
-    complited: bool = False
+    completed: bool = False
 
 class TaskCreate(BaseModel):
     title: str
+
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
 
 tasks: list[Task] = []
 
@@ -31,21 +35,75 @@ def get_tasks():
 
 @app.post('/tasks', response_model=Task, status_code=status.HTTP_201_CREATED)
 def add_task(payload: TaskCreate):
-    task = Task(id=str(uuid4()), title=payload.title, complited=False)
+    task = Task(id=str(uuid4()), title=payload.title, completed=False)
     tasks.append(task)
     return task
 
-book = ''
+@app.patch('/tasks/{task_id}', response_model=Task)
+def patch_task(task_id: str, payload: TaskUpdate):
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title is not None:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
+            return task
 
-class FavoriteBook(BaseModel):
-    book_name: str
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Задача не найдена')
 
-@app.get('/book')
-def get_favorite_book():
-    return f'Любимая книга: {book}'
+@app.delete('/tasks/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: str):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return f'Task has been successful removed'
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Задача не найдена')
 
-@app.post('/book', response_model=FavoriteBook, status_code=status.HTTP_201_CREATED)
-def add_favorite_book(payload: FavoriteBook):
-    global book
-    book = payload.book_name
-    return payload
+
+
+
+
+# -----------------------------
+
+class BaseCategory(BaseModel):
+    name_category: str
+
+class Category(BaseCategory):
+    id: str
+
+class CategoryCreate(BaseCategory):
+    pass
+
+class CategoryUpdate(BaseCategory):
+    pass
+
+categories: list[Category] = []
+
+@app.get('/categories')
+def get_categories():
+    return categories
+
+@app.post('/categories')
+def post_categories(payload: Category):
+    category = Category(id=str(uuid4()), name_category=payload.name_category)
+    categories.append(category)
+    return category
+
+@app.patch('/categories/{category_id}')
+def patch_categories(category_id: str, payload: CategoryUpdate):
+    for category in categories:
+        if category.id == category_id:
+            if payload.name_category is not None:
+                category.name_category = payload.name_category
+            return category
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Категория не найдена')
+
+@app.delete('/categories/{category_id}')
+def delete_category(category_id: str):
+    for category in categories:
+        if category.id == category_id:
+            categories.remove(category)
+            return f'Task has been successful removed'
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Категория не найдена')
